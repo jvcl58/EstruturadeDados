4d76258b79af84ba080efc1073601e3f6ba952b3
João Victor de Carvalho Lima <jvclcontato@gmail.com>
2025-09-05 18:38:03 -0300

    test

--- Slide01/Ex05.java
+++ Slide01/Ex05.java
@@ -1,3 +1,9 @@
+// 5. Implemente dois algoritmos que ordenem os elementos de um vetor em ordem
+//    crescente e compare o número de trocas que eles efetuam durante a
+//    ordenação de:
+// a) um vetor criado com os números de 1 até 100 aleatoriamente distribuídos.
+// b) um vetor criado com os números de 1 até 100 ordenados em ordem decrescente.
+
 public class Ex05 {
     
 }
